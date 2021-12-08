064e8c034b63b46dd4f13ed3b879b038f50e8e34
Jazeb Pervez Butt <6316796+jazebbutt@users.noreply.github.com>
2021-12-09 03:14:54 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+*********************
+we can add the code betweeb 
+<head>
+
+</head>
+
+
+***********************
+
+(function($) {
+    $(document).ready(function() {
+        $('#gtranslate_wrapper .selected a').html('<img data-gt-lazy-src="//josenunezlawfirm.com/wp-content/plugins/gtranslate/flags/32/es.png" height="32" width="32" alt="es" src="//josenunezlawfirm.com/wp-content/plugins/gtranslate/flags/32/es.png"> Spanish');
+    });
+    })( jQuery );
